1e5a167f67cdb19c90a7f8ca32d1c4739010372e
Alexander1987r <alexnext7771987r@rambler.ru>
2024-05-09 18:20:35 +0300

    добавили изменеия в фаил config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig( {
+    base:"/Pet-Planet",
     root:"./src",
     publicDir:"../public",
     build:{
